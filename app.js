bd6d8ab4286dd07f1903c78d0c9adc77418b753c
conceptdecorators <conceptdecorators@email.com>
2020-12-22 15:27:26 -0500

    show.html added with filter tabs

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.nav-menu');
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".nav-menu");
 
-menu.addEventListener('click', function(){
-    menu.classList.toggle('is-active'); 
-    menuLinks.classList.toggle('active');
-})
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+});
+
+$(document).ready(function () {
+  $(".grid-shows").isotope({
+    itemSelector: ".item",
+  });
+  $(".filter-button-group").on("click", "li", function () {
+    var filterValue = $(this).attr("data-filter");
+    $(".grid-shows").isotope({ filter: filterValue });
+    $(".filter-button-group li").removeClass("active");
+    $(this).addClass("active");
+  });
+});
